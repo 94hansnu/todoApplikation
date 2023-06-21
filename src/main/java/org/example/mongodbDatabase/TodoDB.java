719cf8f3053c5443e973bcfb0e88c37b7352a30f
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-06-22 01:34:30 +0200

    --

--- src/main/java/org/example/mongodbDatabase/TodoDB.java
+++ src/main/java/org/example/mongodbDatabase/TodoDB.java
@@ -1,4 +1,71 @@
 package org.example.mongodbDatabase;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.example.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoDB {
+   MongoClient mongoClient;
+   MongoDatabase database;
+   MongoCollection<Document> collection;
+
+   public TodoDB(){
+      mongoClient = MongoClients.create();
+      database = mongoClient.getDatabase("mongodb://localhost:27017");
+      collection = database.getCollection("todos");
+   }
+
+   public void save (Todo todo){
+      Document todoDocument = new Document("id", todo.getId())
+              .append("text", todo.getText())
+              .append("done", todo.isDone())
+              .append("assignedTo", todo.getAssignedTo());
+      collection.insertOne(todoDocument);
+   }
+
+   public Todo findById(String id){
+      Document query = new Document("id", id);
+      Document todoDocument = collection.find(query).first();
+
+      if (todoDocument !=null){
+         String text = todoDocument.getString("text");
+         boolean done = todoDocument.getBoolean("done");
+         String assignedTo = todoDocument.getString("assignedTo");
+         return new Todo(id, text, done, assignedTo);
+      }
+      return null;
+   }
+
+   public List<Todo> findAll(){
+      List<Todo> todos = new ArrayList<>();
+      for (Document todoDocument: collection.find()){
+         String id = todoDocument.getString("id");
+         String text = todoDocument.getString("text");
+         boolean done = todoDocument.getBoolean("done");
+         String assignedTo = todoDocument.getString("assignedTo");
+
+         Todo todo = new Todo(id, text, done, assignedTo);
+         todos.add(todo);
+      }
+      return todos;
+   }
+
+   public void update(Todo todo){
+      Document query = new Document("id", todo.getId());
+      Document update = new Document("$set", new Document("text", todo.getText())
+              .append("done", todo.isDone())
+              .append("assignedTo", todo.getAssignedTo()));
+      collection.updateOne(query, update);
+   }
+
+   public void delete(String id){
+      Document query = new Document("id", id);
+      collection.deleteOne(query);
+   }
 }
--- src/main/java/org/example/mongodbFacade/TodoFacade.java
+++ src/main/java/org/example/mongodbFacade/TodoFacade.java
@@ -1,4 +1,42 @@
 package org.example.mongodbFacade;
 
+import org.example.model.Todo;
+import org.example.mongodbDatabase.TodoDB;
+
+import java.util.List;
+
 public class TodoFacade {
+    TodoDB todoDB;
+
+    public TodoFacade(TodoDB todoDB){
+        this.todoDB= todoDB;
+    }
+
+    public void createTodo(Todo todo){
+        todoDB.save(todo);
+    }
+
+    public Todo getTodoById(String id){
+        return todoDB.findById(id);
+    }
+    public List<Todo> getAllTodos(){
+        return todoDB.findAll();
+    }
+    public void updateTodoText(String id, String newText){
+        Todo todo = todoDB.findById(id);
+        if (todo != null){
+            todo.setText(newText);
+            todoDB.update(todo);
+        }
+    }
+    public void updateTodoStatus(String id, boolean done){
+        Todo todo = todoDB.findById(id);
+        if (todo != null){
+            todo.setDone(done);
+            todoDB.update(todo);
+        }
+    }
+    public void deleteTodo(String id){
+        todoDB.delete(id);
+    }
 }
