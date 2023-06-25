a00a2ecea315209ee7f88a6c99a19a381fa65370
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-06-25 23:07:58 +0200

    --- kommentar

--- src/main/java/org/example/model/Todo.java
+++ src/main/java/org/example/model/Todo.java
@@ -1,11 +1,27 @@
 package org.example.model;
 
+/**
+ * Klassen Todo presenterar en att göra-uppgift
+ */
 public class Todo {
+    /**
+     * Här deklareras instansvariablerna i klassen, id representerar ID:t för att göra-uppgiften,
+     * text innehåller skälva texten för uppgiften, done är en boolesk variabel som anger om uppgiften
+     * är klar eller inte och assignedTo innehåller namnet på personen som uppgiften är tilldelas till.
+     */
     private String id;
     private String text;
     private boolean done;
     private String assignedTo;
 
+    /**
+     * Detta är konstruktorn för klassen och den tar emot värden för alla instansvariabler
+     * och används för att skapa en ny instans av Todo med de angivna värdena.
+     * @param id
+     * @param text
+     * @param done
+     * @param assignedTo
+     */
     public Todo(String id, String text, boolean done, String assignedTo) {
         this.id = id;
         this.text = text;
@@ -13,6 +29,10 @@ public class Todo {
         this.assignedTo = assignedTo;
     }
 
+    /**
+     * Metoder getters och setters för variablerna
+     * @return
+     */
     public String getId() {return id;}
 
     public void setId(String id) {this.id = id;}
@@ -29,6 +49,10 @@ public class Todo {
 
     public void setAssignedTo(String assignedTo) {this.assignedTo = assignedTo;}
 
+    /**
+     * Denna metoden toString används för att skapa en strängrepresentation av objektet av typen Todo.
+     * @return
+     */
     @Override
     public String toString(){
         return "Todo [id=" + id
